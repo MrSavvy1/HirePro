1578b768ee4945ece98eae3f2766b2535367bf12
Saviour Edethu <108595932+MrSavvy1@users.noreply.github.com>
2024-06-15 03:22:19 +0000

    added auth but revert

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,17 @@
 const express = require('express');
+<<<<<<< HEAD
 const router = express.Router();
 const { registerUser, loginUser } = require('../auth/authController');
 
 router.post('/signup', registerUser);
 router.post('/login', loginUser);
+=======
+const { signup, login } = require('../models/auth/authControllers');
+
+const router = express.Router();
+
+router.post('/signup', signup);
+router.post('/login', login);
+>>>>>>> parent of 1a3aa6e (auth added)
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
+// server/server.js
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 
+//dotenv.config();
+
 const app = express();
 
+<<<<<<< HEAD
 // Middleware
+=======
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+	.catch((error) => console.log(error));
+
+app.use(cors());
+>>>>>>> parent of 1a3aa6e (auth added)
 app.use(express.json());
 app.use(cors());
 
