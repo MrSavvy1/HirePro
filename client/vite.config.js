4cb2a8a7c3643f63795d4c7285da0e95d4bd753c
Saviour Edethu <108595932+MrSavvy1@users.noreply.github.com>
2024-06-29 09:36:32 +0000

    deploy to github

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
+import { base: "/client/",
+  defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/deploy-github/",
+  base: "/client/",
   plugins: [react()],
 })
